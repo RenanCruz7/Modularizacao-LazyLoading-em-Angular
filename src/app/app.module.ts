f27294764e6fa7d90d0911a05c9b9e6963a3285b
RenanCruz7 <renancruz199@gmail.com>
2024-08-21 19:37:36 -0300

    Modulo de Home adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
 import { LoginComponent } from './pages/login/login.component';
-
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
 import { BuscaComponent } from './pages/busca/busca.component';
 import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './core/material/material.module';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PromocoesComponent,
-    DepoimentosComponent,
     LoginComponent,
     CadastroComponent,
     PerfilComponent,
@@ -37,7 +30,8 @@ import { MaterialModule } from './core/material/material.module';
     HttpClientModule,
     SharedModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HomeModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
