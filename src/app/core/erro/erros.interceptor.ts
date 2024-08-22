65f73bca41f8bd3a03228fcf692914cce0b3c91d
RenanCruz7 <renancruz199@gmail.com>
2024-08-22 19:54:37 -0300

    Utilizando Interceptor para exibir mensagem

--- src/app/core/erro/erros.interceptor.ts
+++ src/app/core/erro/erros.interceptor.ts
@@ -7,30 +7,34 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
+import { MensagemService } from '../services/mensagem.service';
 
 @Injectable()
 export class ErrosInterceptor implements HttpInterceptor {
+  constructor(private mensagemService: MensagemService) {}
 
-  constructor() {}
-
-  intercept(request: HttpRequest<HttpErrorResponse>, next: HttpHandler): Observable<HttpEvent<HttpErrorResponse>> {
+  intercept(
+    request: HttpRequest<HttpErrorResponse>,
+    next: HttpHandler
+  ): Observable<HttpEvent<HttpErrorResponse>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        let errorMessage = 'Ocorreu um erro desconhecido'
+        let errorMessage = 'Ocorreu um erro desconhecido';
 
         if (error.error instanceof ErrorEvent) {
-          errorMessage = `Erro do Cliente: ${error.error.message}`
+          errorMessage = `Erro do cliente: ${error.error.message}`;
         } else if (error.status === 404) {
-          errorMessage = 'Recurso não encontrado'
+          errorMessage = 'Recurso não encontrado';
         } else if (error.status === 500) {
-          errorMessage = 'Erro interno do servidor'
+          errorMessage = 'Erro interno do servidor';
         } else if (error.status === 401) {
-          errorMessage = 'Não autorizado'
+          errorMessage = 'Você não está autorizado a acessar este recurso';
         }
 
+        this.mensagemService.openSnackBar(errorMessage);
 
-        return throwError(() => new Error('Ops, Ocorreu um erro'))
+        return throwError(() => new Error('Ops, ocorreu um erro'));
       })
-    )
+    );
   }
 }
